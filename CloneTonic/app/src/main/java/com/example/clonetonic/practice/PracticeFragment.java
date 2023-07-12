6289bd7bd7329a8f4d31d54d8caff27c94433e22
615637 <129474590+615637@users.noreply.github.com>
2023-07-12 17:28:14 +0900

    /

--- CloneTonic/app/src/main/java/com/example/clonetonic/practice/PracticeFragment.java
+++ CloneTonic/app/src/main/java/com/example/clonetonic/practice/PracticeFragment.java
@@ -1,5 +1,7 @@
 package com.example.clonetonic.practice;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,11 +17,14 @@ import com.example.clonetonic.databinding.FragmentPracticeBinding;
 public class PracticeFragment extends Fragment {
     FragmentPracticeBinding binding;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPracticeBinding.inflate(inflater, container, false);
+        binding.btnStudio.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), StudioActivity.class);
+            startActivity(intent);
+        });
 
         return binding.getRoot();
     }
--- CloneTonic/app/src/main/java/com/example/clonetonic/practice/StudioActivity.java
+++ CloneTonic/app/src/main/java/com/example/clonetonic/practice/StudioActivity.java
@@ -3,14 +3,35 @@ package com.example.clonetonic.practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.clonetonic.R;
+import com.example.clonetonic.databinding.ActivityStudioBinding;
 
 public class StudioActivity extends AppCompatActivity {
+    ActivityStudioBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_studio);
+        binding = ActivityStudioBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnConnect.setVisibility(View.GONE);
+        binding.btnLock.setVisibility(View.GONE);
+
+        binding.btnBack.setOnClickListener(v -> {
+            finish();
+        });
+        binding.btnUnlock.setOnClickListener(v -> {
+            binding.btnUnlock.setVisibility(View.GONE);
+            binding.btnLock.setVisibility(View.VISIBLE);
+            binding.btnConnect.setVisibility(View.VISIBLE);
+        });
+        binding.btnLock.setOnClickListener(v -> {
+            binding.btnLock.setVisibility(View.GONE);
+            binding.btnUnlock.setVisibility(View.VISIBLE);
+            binding.btnConnect.setVisibility(View.GONE);
+        });
+
     }
 }
