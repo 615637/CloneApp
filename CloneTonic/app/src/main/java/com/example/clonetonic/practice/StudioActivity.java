a34f710762503475353f004257f7835535bf6e0d
615637 <129474590+615637@users.noreply.github.com>
2023-07-13 17:52:42 +0900

    /

--- CloneTonic/app/src/main/java/com/example/clonetonic/practice/StudioActivity.java
+++ CloneTonic/app/src/main/java/com/example/clonetonic/practice/StudioActivity.java
@@ -11,6 +11,8 @@ import com.example.clonetonic.databinding.ActivityStudioBinding;
 public class StudioActivity extends AppCompatActivity {
     ActivityStudioBinding binding;
 
+    int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,7 @@ public class StudioActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         binding.btnConnect.setVisibility(View.GONE);
         binding.btnLock.setVisibility(View.GONE);
+        a = 0;
 
         binding.btnBack.setOnClickListener(v -> {
             finish();
@@ -26,11 +29,17 @@ public class StudioActivity extends AppCompatActivity {
             binding.btnUnlock.setVisibility(View.GONE);
             binding.btnLock.setVisibility(View.VISIBLE);
             binding.btnConnect.setVisibility(View.VISIBLE);
+            binding.tvStudio.setText("Private studio");
+            binding.tvStudioContent.setText("Only for you and those you invite. Change to a public studio by tapping on the lock.");
+            a=1;
         });
         binding.btnLock.setOnClickListener(v -> {
             binding.btnLock.setVisibility(View.GONE);
             binding.btnUnlock.setVisibility(View.VISIBLE);
             binding.btnConnect.setVisibility(View.GONE);
+            binding.tvStudio.setText("Studio");
+            binding.tvStudioContent.setText("Practice publicy for fun or to get feedback.");
+            a=0;
         });
 
     }
